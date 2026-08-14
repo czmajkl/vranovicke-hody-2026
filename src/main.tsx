3969cd447d272532da39c095c1e16f83561e51a5
czmajkl <110931310+czmajkl@users.noreply.github.com>
2026-08-14 14:54:23 +0200

    Load onboarding styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import '@fontsource/cormorant-garamond/600.css'
 import '@fontsource/cormorant-garamond/700.css'
 import App from './App'
 import './styles.css'
+import './onboarding.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
