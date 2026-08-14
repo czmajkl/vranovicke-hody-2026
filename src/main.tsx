569b454ac9c02cd93be8a180ce91cf784d368cb6
czmajkl <110931310+czmajkl@users.noreply.github.com>
2026-08-14 14:47:53 +0200

    Load self-hosted display font

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@fontsource/cormorant-garamond/400.css'
+import '@fontsource/cormorant-garamond/500.css'
+import '@fontsource/cormorant-garamond/600.css'
+import '@fontsource/cormorant-garamond/700.css'
 import App from './App'
 import './styles.css'
 
