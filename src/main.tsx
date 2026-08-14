748308d9b9c10d0a5ebd95d56d48b5f584098c5c
czmajkl <110931310+czmajkl@users.noreply.github.com>
2026-08-14 18:55:17 +0200

    Switch frontend to HodyAppV6

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,13 @@ import '@fontsource/cormorant-garamond/400.css'
 import '@fontsource/cormorant-garamond/500.css'
 import '@fontsource/cormorant-garamond/600.css'
 import '@fontsource/cormorant-garamond/700.css'
-import HodyAppNext from './HodyAppNext'
+import HodyAppV6 from './HodyAppV6'
 import './styles.css'
 import './onboarding.css'
 import './hody-v2.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HodyAppNext />
+    <HodyAppV6 />
   </React.StrictMode>,
 )
