90e33f960e55cb24b627b185ef64af1cb25c6e2c
czmajkl <110931310+czmajkl@users.noreply.github.com>
2026-08-14 16:50:10 +0200

    Switch frontend to unified next app

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,13 @@ import '@fontsource/cormorant-garamond/400.css'
 import '@fontsource/cormorant-garamond/500.css'
 import '@fontsource/cormorant-garamond/600.css'
 import '@fontsource/cormorant-garamond/700.css'
-import HodyApp from './HodyApp'
+import HodyAppNext from './HodyAppNext'
 import './styles.css'
 import './onboarding.css'
 import './hody-v2.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HodyApp />
+    <HodyAppNext />
   </React.StrictMode>,
 )
