283c6b126a6f1d1cc5a4f0b2f8c0048ee99c507d
czmajkl <110931310+czmajkl@users.noreply.github.com>
2026-08-15 16:31:45 +0200

    Load V9 UI hotfix

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import './styles.css'
 import './onboarding.css'
 import './hody-v2.css'
 import './hody-v6-copy.css'
+import './hody-v9-fix.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
