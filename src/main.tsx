a4b283b42fa35c0d923cef662274236e38b8b641
czmajkl <110931310+czmajkl@users.noreply.github.com>
2026-08-15 16:58:03 +0200

    Load V10 legacy copy fix

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import './onboarding.css'
 import './hody-v2.css'
 import './hody-v6-copy.css'
 import './hody-v9-fix.css'
+import './hody-v10-fix.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
