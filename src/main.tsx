f21962525b638a171a85302a51b8ec2c94e13f09
czmajkl <110931310+czmajkl@users.noreply.github.com>
2026-08-15 16:56:51 +0200

    Switch frontend to V10

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import '@fontsource/cormorant-garamond/400.css'
 import '@fontsource/cormorant-garamond/500.css'
 import '@fontsource/cormorant-garamond/600.css'
 import '@fontsource/cormorant-garamond/700.css'
-import HodyAppV9 from './HodyAppV9'
+import HodyAppV10 from './HodyAppV10'
 import './styles.css'
 import './onboarding.css'
 import './hody-v2.css'
@@ -13,6 +13,6 @@ import './hody-v9-fix.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HodyAppV9 />
+    <HodyAppV10 />
   </React.StrictMode>,
 )
